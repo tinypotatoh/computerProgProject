8ad71ea5e082691731a76e0fe0f869e4ff3921c1
tinypotatoh <94938041+tinypotatoh@users.noreply.github.com>
2022-04-05 16:03:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+function openCard1() {
+    document.getElementById("card1").style.display = "block";
+    document.querySelector(".blur").style.display = "block";
+}
+
+function closeCard1() {
+    document.getElementById("card1").style.display = "none";
+    document.querySelector(".blur").style.display = "none";
+}
+
+function openCard2() {
+    document.getElementById("card2").style.display = "block";
+    document.querySelector(".blur").style.display = "block";
+}
+function closeCard2() {
+    document.getElementById("card2").style.display = "none";
+    document.querySelector(".blur").style.display = "none";
+}
+function openCard3() {
+    document.getElementById("card3").style.display = "block";
+    document.querySelector(".blur").style.display = "block";
+}
+
+function closeCard3() {
+    document.getElementById("card3").style.display = "none";
+    document.querySelector(".blur").style.display = "none";
+}
+
+function openCard4() {
+    document.getElementById("card4").style.display = "block";
+    document.querySelector(".blur").style.display = "block";
+}
+function closeCard4() {
+    document.getElementById("card4").style.display = "none";
+    document.querySelector(".blur").style.display = "none";
+}
+
+function openCard5() {
+    document.getElementById("card5").style.display = "block";
+    document.querySelector(".blur").style.display = "block";
+}
+
+function closeCard5() {
+    document.getElementById("card5").style.display = "none";
+    document.querySelector(".blur").style.display = "none";
+}
+
+function openCard6() {
+    document.getElementById("card6").style.display = "block";
+    document.querySelector(".blur").style.display = "block";
+}
+function closeCard6() {
+    document.getElementById("card6").style.display = "none";
+    document.querySelector(".blur").style.display = "none";
+}
+
+
+
